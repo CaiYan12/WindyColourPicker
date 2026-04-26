f566ae2a9353313784ab0c1334951c854e8a8c22
WindowsIt <2265426573@qq.com>
2026-04-26 15:42:02 +0800

    feat: add application icon (res/favicon.png)
    - Add favicon.ico (multi-size) for Windows .exe icon
    - Add resources.qrc to embed favicon.png in Qt resource
    - Add app.rc for Windows resource compiler
    - Set application window icon via QApplication::setWindowIcon

--- WindyColourPicker.pro
+++ WindyColourPicker.pro
@@ -2,8 +2,10 @@ QT       += core gui widgets
 TARGET = WindyColourPicker
 TEMPLATE = app
 
-
 win32: LIBS += -lgdi32 -luser32
+
+RESOURCES += res/resources.qrc
+RC_FILE = res/app.rc
 
 SOURCES += \
     main.cpp \
--- main.cpp
+++ main.cpp
@@ -1,9 +1,11 @@
 #include "WindyColourPicker.h"
 #include <QApplication>
+#include <QIcon>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+    a.setWindowIcon(QIcon(":/favicon.png"));
     WindyColourPicker w;
     w.show();
     return a.exec();
